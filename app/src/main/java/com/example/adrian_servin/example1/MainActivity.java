0a715fac6b4e32b709dfa8ee5bd4f1666597cc8c
Adrian Servin <adrianservin93@gmail.com>
2015-03-26 23:31:05 -0700

    Se agrego todo

---
+++ app/src/main/java/com/example/adrian_servin/example1/MainActivity.java
@@ -0,0 +1,126 @@
+package com.example.adrian_servin.example1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends ActionBarActivity{
+
+    EditText txtNumA, txtNumB;
+    Button btnSum,btnRes,btnMul,btnDiv,btnClean;
+    TextView lblResult;
+    int NumA, NumB, Result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNumA = (EditText) findViewById(R.id.txtNumA);
+        txtNumB = (EditText) findViewById(R.id.txtNumB);
+        btnSum = (Button) findViewById(R.id.btnPlus);
+        btnRes = (Button) findViewById(R.id.btnMin);
+        btnMul = (Button) findViewById(R.id.btnMul);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+        btnClean = (Button) findViewById(R.id.btnClean);
+        lblResult = (TextView) findViewById(R.id.lblResult);
+
+        btnSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              if (String.valueOf(txtNumA.getText()).equals("")
+                      ||String.valueOf(txtNumB.getText()).equals("")) {
+
+                  lblResult.setText("Error: " + "Pon Un Número");
+
+
+              }else{
+                  NumA = Integer.parseInt(String.valueOf(txtNumA.getText()));
+                  NumB = Integer.parseInt(String.valueOf(txtNumB.getText()));
+                  Result = NumA + NumB;
+                  lblResult.setText("Resultado: " + Result);
+              }
+
+
+
+            }
+        });
+
+        btnRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (String.valueOf(txtNumA.getText()).equals("")
+                        ||String.valueOf(txtNumB.getText()).equals("")) {
+
+                    lblResult.setText("Error: " + "Pon Un Número");
+
+
+                }else{
+                    NumA = Integer.parseInt(String.valueOf(txtNumA.getText()));
+                    NumB = Integer.parseInt(String.valueOf(txtNumB.getText()));
+                    Result = NumA - NumB;
+                    lblResult.setText("Resultado: " + Result);
+                }
+            }
+        });
+
+        btnMul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (String.valueOf(txtNumA.getText()).equals("")
+                        ||String.valueOf(txtNumB.getText()).equals("")) {
+
+                    lblResult.setText("Error: " + "Pon Un Número");
+
+
+                }else{
+                    NumA = Integer.parseInt(String.valueOf(txtNumA.getText()));
+                    NumB = Integer.parseInt(String.valueOf(txtNumB.getText()));
+                    Result = NumA * NumB;
+                    lblResult.setText("Resultado: " + Result);
+                }
+            }
+        });
+
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (String.valueOf(txtNumA.getText()).equals("")
+                        ||String.valueOf(txtNumB.getText()).equals("")) {
+
+                    lblResult.setText("Error: " + "Pon Un Número");
+
+
+                }else if (String.valueOf(txtNumB.getText()).equals("0")) {
+
+                    lblResult.setText("Error: " + "Div Entre 0");
+
+                }else{
+
+                    NumA = Integer.parseInt(String.valueOf(txtNumA.getText()));
+                    NumB = Integer.parseInt(String.valueOf(txtNumB.getText()));
+                    Result = NumA / NumB;
+                    lblResult.setText("Resultado: " + Result);
+                }
+            }
+        });
+
+        btnClean.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                txtNumA.setText("");
+                txtNumB.setText("");
+                lblResult.setText("Resultado: ");
+
+            }
+        });
+
+    }
+
+}
